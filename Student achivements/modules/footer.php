18d914dba48a1aa138a047750e3181f9611d3e08
Syed Ismail Zabiulla <syedismailart@gmail.com>
2023-07-11 10:49:02 +0530

    Add files via upload

---
+++ Student achivements/modules/footer.php
@@ -0,0 +1,48 @@
+</div>
+		<footer id="footer">
+			<div id="">
+			
+
+			
+				
+			</p>
+			</div>
+			<div id="links">
+			Developed by Dept. of Artificial Intelligence and Machine Learning, HKBKCE
+				<!--a href="#" taget="_blank">Syed Ismail Zabiulla </!--a> 
+				<a href="#" taget="_blank"> Ansari Shammas</a> 
+				<a-- href="#" taget="_blank"> Mohammed Umar Sujath</a--> 
+			
+			</div>
+			
+		</footer>
+		<style>
+			footer {
+			position: fixed;
+  			bottom: 0;
+  			background-color: #373435;
+  			width: 100%;
+  			text-align: center; 
+  			color: #fff;
+  			padding-bottom: 10px;
+
+			}	
+			
+
+			#footer a{
+				margin-right:10px;
+				text-decoration: none;
+				color: white;
+				padding-left:15px;
+				padding-right:15px;
+				margin-bottom:10px;
+			}
+			footer a:hover{
+				color: white;
+			}
+
+		</style>
+		<script src="js/bootstrap.min.js"></script>
+    <script src="js/custom.js"></script>
+  </body>
+</html>
